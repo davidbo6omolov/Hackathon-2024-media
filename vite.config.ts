428f82e2ab7c6ce7fd73b5beda55dcf0b3c92ee2
davidbogomolov <david.bogomolov2@gmail.com>
2024-02-13 00:11:29 +0200

    fix: hope fixed problems with tailwind on prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
-// @ts-expect-error: Unreachable code error
 import tailwindcss from 'vite-plugin-tailwindcss';
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react(),tailwindcss()],
 })
