b483f8d9bd3b37c6ed2a4287c7314dc7cf251a3c
davidbogomolov <david.bogomolov2@gmail.com>
2024-02-13 00:14:41 +0200

    fix: hope fixed problems with tailwind on prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
-import tailwindcss from 'vite-plugin-tailwindcss';
+import VitePluginTailwindcss from "vite-plugin-tailwindcss";
 import react from '@vitejs/plugin-react'
 
 
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(),VitePluginTailwindcss()],
 })
